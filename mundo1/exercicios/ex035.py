d332a167cbcd469de7387f48e9f2ce456135a0e6
Daiane Ribeiro De Souza <daiane@MacBook-Pro-de-Daiane.local>
2024-03-26 20:25:59 -0300

    ex035 do mundo 1

--- mundo1/exercicios/ex035.py
+++ mundo1/exercicios/ex035.py
@@ -5,7 +5,7 @@ r1 = float(input('Valor da reta 1: '))
 r2 = float(input('Valor da reta 2: '))
 r3 = float(input('Valor da reta 3: '))
 
-if (r1 + r2 < r3) or (r1 + r3 < r2) or (r2 + r3 < r1):
-    print(f'As retas de tamanho {r1}, {r2} e {r3} podem formar um triângulo.')
+if (r1 < r2 + r3) and (r2 < r1 + r3) and (r3 < r1 + r2):
+    print(f'Os segmentos acima formam um triângulo.')
 else:
-    print('As retas informadas não podem formar um triângulo.')
+    print('Os segmentos acima não formam um triângulo.')
